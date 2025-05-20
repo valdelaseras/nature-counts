c05ca5f5d9350380c0bc0f9454fb523c4c81ffe6
valdelaseras <robin.on.branch@gmail.com>
2025-05-20 20:15:15 +1200

    [routing] fleshing out routes and page content

--- src/routes/(authenticated)/lobby/+page.svelte
+++ src/routes/(authenticated)/lobby/+page.svelte
@@ -1,3 +1,45 @@
-<header>
-	<h1>Lobby</h1>
-</header>
+<article>
+	<header>
+		<h1>Lobby</h1>
+	</header>
+
+	<section>
+		<nav>
+			<ul class="tile-list">
+				<li>
+					<a href="/match/new">Start a new match</a>
+				</li>
+				<li>
+					<a href="/match/history">View match history</a>
+				</li>
+			</ul>
+		</nav>
+	</section>
+</article>
+
+<style lang="css">
+	.tile-list {
+			padding-left: 0;
+			list-style: none;
+			display: flex;
+			flex-wrap: wrap;
+			gap: 1rem;
+
+      li {
+          width: calc(50% - 0.5rem);
+
+          a {
+              background-color: aquamarine;
+              width: 100%;
+              display: flex;
+              justify-content: center;
+              padding: 1rem;
+              box-sizing: border-box;
+          }
+
+					a:hover, a:focus {
+              background-color: mediumaquamarine;
+					}
+			}
+	}
+</style>
