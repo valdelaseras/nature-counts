efb3f1ce28eaacc1a993b8443ddd06ae59092d88
valdelaseras <robin.on.branch@gmail.com>
2025-05-27 15:35:26 +1200

    [data-model] update simplify model more, clean up, adjust query, adjust types

--- src/lib/shared/types.ts
+++ src/lib/shared/types.ts
@@ -1,6 +1,5 @@
 export type Match = {
 	id: number;
-	created_by?: string; // user uuid
 	start_date?: Date;
 	end_date?: Date;
 	name?: string;
@@ -11,14 +10,12 @@ export type Observation = {
 	id: number;
 	kingdom: Kingdom;
 	created_at: string; // timestamptz
-	created_by: string; // user uuid
 	match: string; // match id
 	user: User;
 };
 
 type User = {
 	id: number;
-	uuid: string;
 	name: string;
 };
 
